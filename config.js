ce650784bf7b8234cbda6e6c39bd45e34232a51f
SamuelBaronAbad <peterpunkbaron@gmail.com>
2021-01-07 16:40:36 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+const PORT_DB = 27017;
+const PORT = process.env.PORT || 3977;
+
+module.exports = {
+    API_VERSION,
+    IP_SERVER,
+    PORT_DB,
+    PORT
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const { API_VERSION, IP_SERVER, PORT_DB, PORT } = require("./config");
+
+mongoose.set("useFindAndModify", false);
+mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/samuelbaronabad`,
+    { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("La conexión a la BBDD es correcta");
+
+            app.listen(port, () => {
+                console.log("######################");
+                console.log("###### API REST ######");
+                console.log("######################");
+                console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}/`);
+            })
+        }
+    }
+);
+
